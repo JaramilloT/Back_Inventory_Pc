22995a6deeebc71abaab67a9f0ab3f282482c922
flaquito16 <sebastian.agudelo0531@gmail.com>
2024-11-05 15:06:21 -0500

    parte final

--- src/main.ts
+++ src/main.ts
@@ -2,48 +2,46 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import * as express from 'express'; // Importa express
-import * as path from 'path'; // Importa path
+import * as express from 'express';
+import * as path from 'path';
+import * as dotenv from 'dotenv';
+
+dotenv.config(); // Cargar variables de entorno desde .env
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001', // Cambia esto por la URL de tu frontend
+    origin: process.env.FRONTEND_URL, // Usa la URL del frontend desde el archivo .env
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Si necesitas permitir cookies
+    credentials: true,
   });
 
-  // Establecer prefijo global para las rutas de la API
   app.setGlobalPrefix('api/v1');
 
-  // Configurar pipes de validación globales
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,           // Ignora propiedades no definidas en el DTO
-      forbidNonWhitelisted: true, // Rechaza propiedades no definidas en el DTO
-      transform: true,           // Transforma la entrada a los tipos esperados
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
 
-  // Middleware para servir archivos estáticos desde la carpeta 'uploads'
   app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
-  // Configuración de Swagger para documentación de la API
   const config = new DocumentBuilder()
-    .setTitle('EMCA')             // Título de la documentación
-    .setDescription('PCS')        // Descripción breve
-    .setVersion('1.0')             // Versión de la API
-    .addTag('EMCA')               // Etiquetas para organización en Swagger
-    .addBearerAuth()               // Añadir autenticación Bearer
+    .setTitle('EMCA')
+    .setDescription('PCS')
+    .setVersion('1.0')
+    .addTag('EMCA')
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('EMCA', app, document);
 
-  // Inicia la aplicación en el puerto definido en la variable de entorno o en el puerto 3000
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0'); // Escuchar en 0.0.0.0 para acceso desde cualquier IP local
 }
 
 bootstrap();
